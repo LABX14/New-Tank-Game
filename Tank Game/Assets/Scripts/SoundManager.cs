be23aabe82728bf3231c1600f22332dfc1ab27ff
Unknown <kaulee@uat.edu>
2021-12-22 17:04:47 -0800

    Added In Audio and Audio Slider
    Still need to add functionality for the slider

---
+++ Tank Game/Assets/Scripts/SoundManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField] Slider volumeSlider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Changes the volume of the audio 
+    public void ChangeVolume()
+    {
+        AudioListener.volume = volumeSlider.value;
+    }
+}
