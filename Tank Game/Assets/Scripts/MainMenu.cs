2a3276bb493433f377b24de7914e5e9d5ce4259d
Unknown <kaulee@uat.edu>
2021-12-23 17:44:02 -0800

    Completed functionality of Audio Slider

--- Tank Game/Assets/Scripts/MainMenu.cs
+++ Tank Game/Assets/Scripts/MainMenu.cs
@@ -5,17 +5,20 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+
     // This will load into the next scene
     public void PlayGame ()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
+    // This will display the options menu and hide the main menu
     public void GoToOptionsMenu ()
     {
         SceneManager.LoadScene("OptionsMenu");
     }
 
+    // This will take the player back to the main menu 
     public void GoToMainMenu()
     {
         SceneManager.LoadScene("MainMenu");
--- Tank Game/Assets/Scripts/SoundManager.cs
+++ Tank Game/Assets/Scripts/SoundManager.cs
@@ -9,12 +9,35 @@ public class SoundManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // If there isn't any saved data from the previous game session, set the value to 1
+        if(!PlayerPrefs.HasKey("musicVolume"))
+        {
+            PlayerPrefs.SetFloat("musicVolume", 1);
+            Load();
+        }
+
+        else
+        {
+            Load();
+        }
     }
 
-    // Changes the volume of the audio 
+    // Sets the volume of the audio to match the value of the slider
     public void ChangeVolume()
     {
         AudioListener.volume = volumeSlider.value;
+        Save();
+    }
+
+    // This will get the value of the slider, that was set to a float
+    private void Load()
+    {
+        volumeSlider.value = PlayerPrefs.GetFloat("musicVolume");
+    }
+
+    // This will take the value set on the volume sider and save it
+    private void Save()
+    {
+        PlayerPrefs.SetFloat("musicVolume", volumeSlider.value);
     }
 }
